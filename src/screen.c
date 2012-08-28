23a4d31ff8b2ebd953a2a756eb572d328eebe830
Clarence Chuahuico <cchuahuico@gmail.com>
2012-08-28 12:24:06 -0700

    Organized a bit

--- src/screen.c
+++ src/screen.c
@@ -2,6 +2,12 @@
 #include "common.h"
 
 #define CALCOFFSET(x, y) ((y) * 80 + (x))
+#define FRAMEWIDTH 80
+#define FRAMEHEIGHT 25
+#define FRAMESIZE (80 * 25)
+#define FRAMEADDR 0xB8000
+#define VGACTRLREG 0x3D4
+#define VGADATAREG 0x3D5
 
 static unsigned short *frame_buffer = FRAMEADDR;
 static unsigned char cur_x;
@@ -10,20 +16,21 @@ static unsigned char cur_y;
 static void update_cursor(void)
 {
     unsigned short loc = CALCOFFSET(cur_x, cur_y);
-    io_out_byte(0x3D4, 14);
-    io_out_byte(0x3D5, loc >> 8);
-    io_out_byte(0x3D4, 15);
-    io_out_byte(0x3D5, loc);
+    io_out_byte(VGACTRLREG, 14);
+    io_out_byte(VGADATAREG, loc >> 8);
+    io_out_byte(VGACTRLREG, 15);
+    io_out_byte(VGADATAREG, loc);
 }
 
 void screen_putc(unsigned char character)
 {
     unsigned short data = character;
-    data &= 0x0FFF;
-    data |= 0x0F00;
+    data &= 0x0FFF; /* black background */
+    data |= 0x0F00; /* white foreground */
 
     frame_buffer[CALCOFFSET(cur_x, cur_y)] = data;
 
+    /* go to next line if whole line is taken */
     if (++cur_x == 80)
     {
         cur_x = 0;
