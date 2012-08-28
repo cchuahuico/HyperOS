5119c13f6b49a5d3147829fd7eb13d137f93970e
Clarence Chuahuico <cchuahuico@gmail.com>
2012-08-28 16:38:15 -0700

    Added some standard typedefs

--- src/common.c
+++ src/common.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-void io_out_byte(unsigned short port, unsigned char data)
+void io_out_byte(u16int port, u8int data)
 {
     asm volatile("outb %1, %0" ::
                  "d" (port), "a" (data));
--- src/common.h
+++ src/common.h
@@ -1,6 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-void io_out_byte(unsigned short port, unsigned char data);
+typedef unsigned int u32int;
+typedef unsigned char u8int;
+typedef unsigned short u16int;
+
+void io_out_byte(u16int port, u8int data);
 
 #endif
