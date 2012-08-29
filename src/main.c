f297d9f0654b0f96f355c8cb44b5e7519dc5f288
Clarence Chuahuico <cchuahuico@gmail.com>
2012-08-28 23:52:55 -0700

    Added code for testing interrupts

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "screen.h"
 #include "gdt.h"
+#include "isr.h"
 
 int main(void)
 {
     init_gdt();
+    init_idt();
     screen_clear();
     screen_puts("hello, world!");
     return 0;
