da099dd3956fabb7d15d946daa235c7630b8d6f9
Clarence Chuahuico <cchuahuico@gmail.com>
2012-08-29 15:13:59 -0700

    Testing interrupt handling

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ int main(void)
     init_gdt();
     init_idt();
     screen_clear();
-    screen_puts("hello, world!");
+    screen_puts("hello, world!\n");
+    asm  volatile ("int $0x10");
     return 0;
 }
